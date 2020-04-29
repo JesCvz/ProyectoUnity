f6fe9101d4df19855d2fa75f55100906dea87832
JesCvz <jes_cvz@hotmail.com>
2020-04-29 15:46:44 -0500

    Serialize for levers in Inspector

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float MainThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
     Rigidbody rigidBody;
     AudioSource audioSource;
     // Start is called before the first frame update
@@ -22,9 +24,10 @@ public class Rocket : MonoBehaviour
     }
     private void Thrust()
     {
+        float ThrustthisFrame = MainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up*ThrustthisFrame);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -39,14 +42,17 @@ public class Rocket : MonoBehaviour
     private void Rotate()
     {
         rigidBody.freezeRotation = true; //take manual rotation
+
         
+        float rotationthisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationthisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward*rotationthisFrame);
         }
         rigidBody.freezeRotation = false; //resume physics control of rotation
     }
