50376b2c0fdf420eb809b060af596f88ed1612e8
JesCvz <jes_cvz@hotmail.com>
2020-05-02 04:22:35 -0500

    Camera Following and Level Improvements

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -11,6 +11,8 @@ public class CameraFollow : MonoBehaviour
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
         transform.position = desiredPosition;
+
+        transform.LookAt(target);
     }
 }
 
